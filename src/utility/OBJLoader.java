b47ba5aa1666d99aae5fe370840205fbcb959462
finnhartshorn <finnhartshorn@gmail.com>
2017-10-16 19:57:02 +1100

    Moved vector functions to seperate file.

--- src/utility/OBJLoader.java
+++ src/utility/OBJLoader.java
@@ -8,7 +8,9 @@ import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
-import com.jogamp.opengl.math.VectorUtil;
+import static utility.Vector3f.calculateNormal;
+import static utility.Vector3f.divideVector;
+import static utility.Vector3f.sumVectors;
 
 public class OBJLoader {
 
@@ -106,50 +108,5 @@ public class OBJLoader {
         List<String> face = new ArrayList<>(Arrays.asList(line.split(" ")));
         face.remove(0);
         return face.stream().mapToInt(Integer::parseInt).toArray();
-    }
-
-    public static float[] calculateNormal(ArrayList<float[]> face) {
-        float[] normalVector = { 0f, 0f, 0f };
-        float[] a = { 0f, 0f, 0f };
-        float[] b = { 0f, 0f, 0f };
-
-
-        a[0] = face.get(0)[0] - face.get(1)[0];
-        a[1] = face.get(0)[1] - face.get(1)[1];
-        a[2] = face.get(0)[2] - face.get(1)[2];
-
-        b[0] = face.get(1)[0] - face.get(2)[0];
-        b[1] = face.get(1)[1] - face.get(2)[1];
-        b[2] = face.get(1)[2] - face.get(2)[2];
-
-        normalVector[0] = (a[1] * b[2]) - (a[2] * b[1]);
-        normalVector[1] = (a[2] * b[0]) - (a[0] * b[2]);
-        normalVector[2] = (a[0] * b[1]) - (a[1] * b[0]);
-
-        normalVector = normalize(normalVector);
-
-        return normalVector;
-    }
-
-    public static float[] normalize(float[] vector) {
-        float length = (float)Math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]) + (vector[2] * vector[2]));
-
-        if (length == 0.0f) {
-            length = 1.0f;
-        }
-
-        vector[0] /= length;
-        vector[1] /= length;
-        vector[2] /= length;
-
-        return vector;
-    }
-
-    public static float[] sumVectors(float[] vector1, float[] vector2) {
-        return new float[] { vector1[0] + vector2[0], vector1[0] + vector2[0], vector1[0] + vector2[0] };
-    }
-
-    public static float[] divideVector(float[] vector, float divisor) {
-        return new float[] { vector[0]/divisor, vector[1]/divisor, vector[2]/divisor };
     }
 }
---
+++ src/utility/Vector3f.java
@@ -0,0 +1,51 @@
+package utility;
+
+import java.util.ArrayList;
+
+public class Vector3f {
+
+    public static float[] calculateNormal(ArrayList<float[]> face) {
+        float[] normalVector = { 0f, 0f, 0f };
+        float[] a = { 0f, 0f, 0f };
+        float[] b = { 0f, 0f, 0f };
+
+
+        a[0] = face.get(0)[0] - face.get(1)[0];
+        a[1] = face.get(0)[1] - face.get(1)[1];
+        a[2] = face.get(0)[2] - face.get(1)[2];
+
+        b[0] = face.get(1)[0] - face.get(2)[0];
+        b[1] = face.get(1)[1] - face.get(2)[1];
+        b[2] = face.get(1)[2] - face.get(2)[2];
+
+        normalVector[0] = (a[1] * b[2]) - (a[2] * b[1]);
+        normalVector[1] = (a[2] * b[0]) - (a[0] * b[2]);
+        normalVector[2] = (a[0] * b[1]) - (a[1] * b[0]);
+
+        normalVector = normalize(normalVector);
+
+        return normalVector;
+    }
+
+    public static float[] normalize(float[] vector) {
+        float length = (float)Math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]) + (vector[2] * vector[2]));
+
+        if (length == 0.0f) {
+            length = 1.0f;
+        }
+
+        vector[0] /= length;
+        vector[1] /= length;
+        vector[2] /= length;
+
+        return vector;
+    }
+
+    public static float[] sumVectors(float[] vector1, float[] vector2) {
+        return new float[] { vector1[0] + vector2[0], vector1[0] + vector2[0], vector1[0] + vector2[0] };
+    }
+
+    public static float[] divideVector(float[] vector, float divisor) {
+        return new float[] { vector[0]/divisor, vector[1]/divisor, vector[2]/divisor };
+    }
+}
